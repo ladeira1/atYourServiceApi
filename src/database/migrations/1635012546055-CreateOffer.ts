95169ef8502ccc320154f4289e219873fe37dfb5
Joao Ladeira <joaoladeirag@gmail.com>
2021-10-23 15:14:44 -0300

    Create Offer migration

---
+++ src/database/migrations/1635012546055-CreateOffer.ts
@@ -0,0 +1,84 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateOffer1635012546055 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'offer',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'message',
+            type: 'varchar',
+          },
+          {
+            name: 'value',
+            type: 'numeric',
+          },
+          {
+            name: 'status',
+            type: 'boolean',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'user',
+            type: 'uuid',
+          },
+          {
+            name: 'service',
+            type: 'integer',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'offer',
+      new TableForeignKey({
+        columnNames: ['service'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'service',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'offer',
+      new TableForeignKey({
+        columnNames: ['user'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'user',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('offer');
+  }
+}
